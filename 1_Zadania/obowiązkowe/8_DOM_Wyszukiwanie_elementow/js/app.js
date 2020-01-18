8581ac4c934e21b88b0aab0c4b6ed376cb9bb787
Sebastian Ślipek <sebastian.slipek@microsoft.wsei.edu.pl>
2020-01-18 18:26:05 +0100

    solution to the task 8

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,4 +3,35 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
 
+     // Zadanie 0
+
+     const classElTitle = document.querySelector('.title');
+     function getDataAnimation(element) {
+        const newValue = element.dataset.animation;
+        return newValue;
+     }
+     console.log(getDataAnimation(classElTitle));
+
+     // Zadanie 1
+
+     const elementIdHome = document.querySelector('#home');
+     const firstLiDataDirectionEl = document.querySelector('li[data-direction]');
+     const firstElClassBlock = document.querySelector('.block');
+
+     // Zadanie 2
+
+     const allLiElementsInTagNav = document.querySelectorAll('nav li');
+     const allPElementsInDivElements = document.querySelectorAll('div p');
+     const allDivsInArticleTag = document.querySelectorAll('article div');
+
+     console.log(allLiElementsInTagNav);
+     console.log(allPElementsInDivElements);
+     console.log(allDivsInArticleTag);
+
+     // Zadanie 3
+
+     const articleFirstClassElement = document.querySelector('article.first');
+     console.log(articleFirstClassElement);
+     let h1Elements = articleFirstClassElement.getElementsByTagName('h1');
+     console.log(h1Elements.length);
 });
